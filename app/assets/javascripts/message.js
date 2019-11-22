8873296d421847dcd19180149d7ccfbd6ec0b7b3
hataemon <hataemon911@gmail.com>
2019-11-22 12:56:54 +0900

    search

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,13 +2,17 @@ $(function(){
 
 
 
-
-  var buildMessageHTML = function(message) {
-    if (message.content && message.image.url) {
-      //data-idが反映されるようにしている
-      var html = `<div class="message" data-id=` + message.id + `>` +
-        `<div class="upper-message">` +
-          `<div class="upper-message__user-name">` +
+  var buildMessgeInContentAndImage = function(message) {
+    return `${
+      (message.content)? (
+        `<p class="lower-message__content">` +
+          message.content +
+        `</p>`
+      ) : ''
+    }` 
+     '<div class="message" data-id=` + message.id + '>'' +
+        '<div class="upper-message">' +
+          '<div class="upper-message__user-name">' +
             message.user_name +
           `</div>` +
           `<div class="upper-message__date">` +
@@ -16,12 +20,19 @@ $(function(){
           `</div>` +
         `</div>` +
         `<div class="lower-message">` +
-          `<p class="lower-message__content">` +
+          (message.content)? (`<p class="lower-message__content">` +
             message.content +
-          `</p>` +
-          `<img src="` + message.image.url + `" class="lower-message__image" >` +
+          `</p>`) : '' +
+          (`<img src="` + message.image.url + `" class="lower-message__image" >`) +
         `</div>` +
       `</div>`
+  }
+
+  var buildMessageHTML = function(message) {
+    html = (true)? 1 : 0 ;
+    if (message.content && message.image.url) {
+      //data-idが反映されるようにしている
+      var html = buildMessgeInContentAndImage(message)
     } else if (message.content) {
       //同様に、data-idが反映されるようにしている
       var html = `<div class="message" data-id=` + message.id + `>` +
@@ -57,45 +68,11 @@ $(function(){
     };
     return html;
   };
-//省略
-続いて、reloadMessages関数からもHTMLを組み立てる関数を呼ぶようにします。
-
-message.jsを以下のように編集しましょう
-以下の処理が書かれていない部分は、ご自身で考えて書いてみましょう。
 
 message.js
- 1
- 2
- 3
- 4
- 5
- 6
- 7
- 8
- 9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
+ 
 $(function() {
-//省略
+
 
   var reloadMessages = function() {
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
@@ -124,40 +101,3 @@ $(function() {
     });
   };
 });
-
-
-
-
-
-// function buildPost(message){
-//  var html = '<div class = "content">
-//               ${message.text}
-//               </div>'
-//  return html;
-// }
-
-//   $('.new_message').on('submit', function(e){
-//     e.preventDefault(e);
-//     var formData = new FormData(this);
-//     var url = $(this).attr('action');
-//     $.ajax({
-//       url: url,  
-//       type: 'POST', 
-//       data: formData,
-//       dataType: 'json',
-//       processData: false,
-//       contentType: false
-//     })
-
-//     function resetForm(message){
-//       document.getElementById('.contents').submit();
-//     }
-//     function resetForm(message){
-//      document.getElementById('#message_text').submit();
-//     }
-
-//     .fail(function(){
-//       alert("メッセージ送信に失敗しました");
-//     })
-//   })
-// })
