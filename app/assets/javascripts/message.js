6d28e32f13fca0f5449dc5baf79cce4faf0084d9
hataemon <hataemon911@gmail.com>
2019-11-22 14:14:38 +0900

    search

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,69 +3,69 @@ $(function(){
 
 
   var buildMessgeInContentAndImage = function(message) {
-    return `${
+    return '${
       (message.content)? (
-        `<p class="lower-message__content">` +
-          message.content +
-        `</p>`
+        '<p class="lower-message__content">'
+          message.content 
+        '</p>'
       ) : ''
-    }` 
-     '<div class="message" data-id=` + message.id + '>'' +
-        '<div class="upper-message">' +
-          '<div class="upper-message__user-name">' +
-            message.user_name +
-          `</div>` +
-          `<div class="upper-message__date">` +
-            message.created_at +
-          `</div>` +
-        `</div>` +
-        `<div class="lower-message">` +
-          (message.content)? (`<p class="lower-message__content">` +
-            message.content +
-          `</p>`) : '' +
-          (`<img src="` + message.image.url + `" class="lower-message__image" >`) +
-        `</div>` +
-      `</div>`
+    }' 
+     '<div class="message" data-id=' + message.id  '>'' 
+        '<div class="upper-message">' 
+          '<div class="upper-message__user-name">' 
+            message.user_name 
+          '</div>'
+          '<div class="upper-message__date">' 
+            message.created_at 
+          '</div>' 
+        '</div>` 
+        '<div class="lower-message">' 
+          (message.content)? ('<p class="lower-message__content">'
+            message.content 
+          `</p>`) : '' 
+          ('<img src="' + message.image.url + ''" class="lower-message__image" >') 
+        '</div>'
+      '</div>''
   }
 
   var buildMessgeInContentAndImage = function(message) {
-    return `${
+    return '${
       (message.content)? (
-        `<p class="lower-message__content">` +
-          message.content +
-        `</p>`
+        '<p class="lower-message__content">' 
+          message.content 
+        '</p>'
       ) : ''
-    }` 
-      var html = `<div class="message" data-id=` + message.id + `>` +
-      `<div class="upper-message">` +
-          `<div class="upper-message__user-name">` +
-            message.user_name +
-          `</div>` +
-          `<div class="upper-message__date">` +
+    }'
+      var html = '<div class="message" data-id=' + message.id  '>'
+            '<div class="upper-message">'
+          '<div class="upper-message__user-name">'
+            message.user_name 
+          '</div>'
+          '<div class="upper-message__date">'
             message.created_at +
-          `</div>` +
-        `</div>` +
-        `<div class="lower-message">` +
-          `<p class="lower-message__content">` +
-            message.content +
-          `</p>` +
-        `</div>` +
-      `</div>`
+          '</div>'
+        '</div>'
+        '<div class="lower-message">'
+          '<p class="lower-message__content">'
+            message.content
+          '</p>'
+        '</div>'
+      '</div>'
     } else if (message.image.url) {
       //同様に、data-idが反映されるようにしている
-      var html = `<div class="message" data-id=` + message.id + `>` +
-        `<div class="upper-message">` +
-          `<div class="upper-message__user-name">` +
-            message.user_name +
-          `</div>` +
-          `<div class="upper-message__date">` +
-            message.created_at +
-          `</div>` +
-        `</div>` +
-        `<div class="lower-message">` +
-          `<img src="` + message.image.url + `" class="lower-message__image" >` +
-        `</div>` +
-      `</div>`
+      var html = '<div class="message" data-id=' message.id  ''>'
+        '<div class="upper-message">'
+          '<div class="upper-message__user-name">'
+            message.user_name 
+          '</div>'
+          '<div class="upper-message__date">'
+            message.created_at 
+          '</div>'
+        '</div>'
+        '<div class="lower-message">'
+          '<img src="' message.image.url  ''" class="lower-message__image" >'
+        '</div>'
+      '</div>'
     };
     return html;
   };
