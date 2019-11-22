faf1f75d8270993d03977989b54c6d90bb0029ad
hataemon <hataemon911@gmail.com>
2019-11-22 14:37:15 +0900

    search

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -42,7 +42,7 @@ $(function(){
             message.user_name 
           '</div>'
           '<div class="upper-message__date">'
-            message.created_at +
+            message.created_at 
           '</div>'
         '</div>'
         '<div class="lower-message">'
