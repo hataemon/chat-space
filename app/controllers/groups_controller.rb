f084f0a6a2af846d880a9f0f1a9d6b74828bcf72
hataemon <hataemon911@gmail.com>
2019-11-27 00:43:17 +0900

    master

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,6 +18,16 @@ class GroupsController < ApplicationController
     end
   end
 
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
+  end
+
+
   def update
     if @group.update(group_params)
       redirect_to group_messages_path, notice: 'グループを更新しました'
--- app/models/group.rb
+++ app/models/group.rb
@@ -5,6 +5,7 @@ class Group < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   
+
   def show_last_message
     if (last_message = messages.last).present?
       last_message.content? ? last_message.content : '画像が投稿されています'
@@ -13,3 +14,29 @@ class Group < ApplicationRecord
     end
   end
 end
+
+
+
+
+  #   def show_last_message
+  #   if (last_message = messages.last).present?
+  #     if last_message.content? 
+  #      last_message.content 
+  #   else
+  #     '画像が投稿されています'
+  #   end
+  #   else 
+  #   'まだメッセージはありません。'
+  #   end
+  # end
+
+
+
+
+
+  # def show_last_message
+  #   if (last_message = messages.last).present?
+  #     last_message.content? ? last_message.content : '画像が投稿されています'
+  #   else
+  #     'まだメッセージはありません。'
+  #   end
